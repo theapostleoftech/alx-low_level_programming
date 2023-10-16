a798a69c42eab393ebfac8fb880f52f3f39a0ab8
Chukwunonso Nwankpa <chukwunonsonwankpa@gmail.com>
2023-10-16 13:25:06 -0700

    a function that returns the length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strlen : retuns the length of a string
+ * @s : string parameter
+ *
+ * Return: 0.
+ */
+int _strlen(char *s)
+{
+char str = *str;
+str = "I love ALX";
+ *str = str;
+}
