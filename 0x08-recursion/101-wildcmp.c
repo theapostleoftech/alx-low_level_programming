7d2c21cc589e05a78505a7ec4a468f34fd652a04
Chukwunonso Nwankpa <chukwunonsonwankpa@gmail.com>
2023-10-25 17:17:42 -0700

    a function that compares two strings and returns 1 if the strings can be considered identical, otherwise return 0.

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,36 +1,30 @@
 #include "main.h"
-#include <stdio.h>
-
 /**
- * wildcmp - compares two strings
- * @s1: special character
- * @s2: special character to be checked
+ * wildcmp - Compares two strings with wildcard '*'.
+ * @s1: The first string.
+ * @s2: The second string with '*' as a wildcard.
  *
- * Return: string value
+ * Return: 1 if the strings can be considered identical, 0 otherwise.
  */
 int wildcmp(char *s1, char *s2) {
+    /* If we've reached the end of both strings, they are identical. */
     if (*s1 == '\0' && *s2 == '\0') {
         return 1;
     }
 
-    if (*s2 == '*' || (*s1 == *s2 && *s1 != '\0')) {
-        return wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1);
+    /* If the current characters match, move to the next character in both strings. */
+    if (*s1 == *s2) {
+        return wildcmp(s1 + 1, s2 + 1);
     }
 
-    return 0;
-}
-
-int main() {
-    char str1[] = "hello";
-    char str2[] = "h*o";
-    
-    int result = wildcmp(str1, str2);
-    
-    if (result == 1) {
-        printf("Strings are identical.\n");
-    } else {
-        printf("Strings are not identical.\n");
+    /* If s2 contains a '*', try two possibilities:
+     * 1. '*' represents an empty string in s1 (skip '*').
+     * 2. Match the current character of s1 with the '*' in s2.
+     */
+    if (*s2 == '*') {
+        return wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2);
     }
 
+    /* If none of the above conditions met, the strings are not identical. */
     return 0;
 }
