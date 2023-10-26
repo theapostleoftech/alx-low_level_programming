39c6d1769dbb358af4c425df47e5add6795bdaf7
Chukwunonso Nwankpa <chukwunonsonwankpa@gmail.com>
2023-10-25 17:10:56 -0700

    a function that compares two strings and returns 1 if the strings can be considered identical, otherwise return 0.

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -9,21 +9,14 @@
  * Return: string value
  */
 int wildcmp(char *s1, char *s2) {
-    // If both strings are empty, they are considered identical.
     if (*s1 == '\0' && *s2 == '\0') {
         return 1;
     }
 
-    // If the current characters match, or s2 contains a wildcard '*',
-    // you have two options: either advance in s1 or s2.
     if (*s2 == '*' || (*s1 == *s2 && *s1 != '\0')) {
-        // Try to match the current character in s1 with '*' in s2
-        // or move to the next character in s1.
         return wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1);
     }
 
-    // If the current characters don't match and there's no '*',
-    // the strings cannot be considered identical.
     return 0;
 }
 
