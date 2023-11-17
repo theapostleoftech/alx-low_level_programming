d409cae8dc4a7f49508c25e6b8d033b0c16c7c2c
Chukwunonso Nwankpa <chukwunonsonwankpa@gmail.com>
2023-11-16 17:55:46 -0800

    a 64-bit program in assembly that prints Hello, Holberton, followed by a new line

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,20 @@
+ection .data
+hello db 'Hello, Holberton', 0; our string
+format db '%s', 10, 0; format for printf
+
+section .text
+global main
+extern printf
+
+main:
+; push parameter for printf in reverse order
+push format
+push hello
+
+; call printf
+call printf
+add exp, 8 ; clean up stack
+
+, return 0
+xor eax, eax
+ret
