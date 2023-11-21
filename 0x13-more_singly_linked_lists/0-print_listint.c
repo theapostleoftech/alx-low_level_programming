b44fd2448fc78ea74195d47cf263a8e30c0d583d
Chukwunonso Nwankpa <chukwunonsonwankpa@gmail.com>
2023-11-21 15:44:58 -0800

    a function that prints all the elements of a listint_t list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,19 @@
+#inlcude "lists.h"
+/**
+ * print_listint - prints a list
+ * @h: a list to be printed
+ *
+ * Return: the element linked
+ */
+size_t print_listint(const listint_t *h)
+{
+	int a = 0;
+
+	while (h != NULL)
+	{
+		printf("%i\n", h->n);
+		h = h->next;
+		a++;
+	}
+	return (a);
+}
