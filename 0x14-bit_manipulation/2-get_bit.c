570cbbd7389017be71605dbdebb4febb1fa4c0d5
Chukwunonso Nwankpa <chukwunonsonwankpa@gmail.com>
2023-11-24 00:55:18 +0100

    a function that returns the value of a bit at a given index

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -3,7 +3,7 @@
  * get_bit - return the value of bit
  * @n: integer number
  * @index: the index starting from 0
- * 
+ *
  * Return: the value of bit at index or -1
  */
 int get_bit(unsigned long int n, unsigned int index)
