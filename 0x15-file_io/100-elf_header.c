87f8a2944fd207ad351e46ea6daf80b4562ec76a
Chukwunonso Nwankpa <chukwunonsonwankpa@gmail.com>
2023-11-28 04:31:58 +0100

    a program that displays the information contained in the ELF header at the start of an ELF file

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,15 +1,7 @@
-/*
- * File: 100-elf_header.c
- * Auth: Brennan D Baraban
- */
-
+#include "main.h"
 #include <elf.h>
 #include <sys/types.h>
 #include <sys/stat.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
 
 void check_elf(unsigned char *e_ident);
 void print_magic(unsigned char *e_ident);
