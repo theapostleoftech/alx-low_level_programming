3640c145bebdcd3a8d27dda49bce384588d9a3f9
Chukwunonso Nwankpa <chukwunonsonwankpa@gmail.com>
2024-02-02 23:57:02 +0100

    a function that deletes a hash table

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -7,7 +7,7 @@
 void hash_table_delete(hash_table_t *ht)
 {
 	hash_table_t *head = ht;
-	hash_node_t *node, *tmp;
+	hash_node_t *node, *temp;
 	unsigned long int i;
 
 	for (i = 0; i < ht->size; i++)
@@ -17,11 +17,11 @@ void hash_table_delete(hash_table_t *ht)
 			node = ht->array[i];
 			while (node != NULL)
 			{
-				tmp = node->next;
+				temp = node->next;
 				free(node->key);
 				free(node->value);
 				free(node);
-				node = tmp;
+				node = temp;
 			}
 		}
 	}
