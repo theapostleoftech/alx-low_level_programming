0170e94e61ac8424b946ca4f74ff5a716ec75677
Chukwunonso Nwankpa <chukwunonsonwankpa@gmail.com>
2024-05-08 18:17:21 +0000

    Binary search

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -6,7 +6,6 @@
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
-int binary_search_index(int *array, size_t l, size_t r, int value);
 void print_array(int *array, size_t l, size_t r, int value);
 
 #endif
